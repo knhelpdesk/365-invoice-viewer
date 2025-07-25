3d3298627132f8c0bfc7efbd33076a60a36b00a3
Kerner Norland <gcp@kernernorland.com>
2025-07-25 07:42:32 +0530

    Updated InvoiceViewer.tsx

--- client/src/components/InvoiceViewer.tsx
+++ client/src/components/InvoiceViewer.tsx
@@ -1,12 +1,26 @@
 @@ .. @@
        const response = await fetch('http://localhost:3001/api/tenants', {
+       }
+       )
 -      const response = await fetch('/api/tenants', {
          headers: {
+         }
+}
+)
 @@ .. @@
        const response = await fetch(`http://localhost:3001/api/invoices?${params}`, {
+       }
+       )
 -      const response = await fetch(`/api/invoices?${params}`, {
          headers: {
+         }
+}
+)
 @@ .. @@
        const response = await fetch(`http://localhost:3001/api/invoices/${invoiceId}/download`, {
+       }
+       )
 -      const response = await fetch(`/api/invoices/${invoiceId}/download`, {
          headers:
+}
+)
